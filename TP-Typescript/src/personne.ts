36bf66bb0d344c76a2ff6fb6253b6a4aba651b31
rasheed <rasheedzein7557@gmail.com>
2019-09-16 14:33:13 +0200

    file personne modified

--- TP-Typescript/src/personne.ts
+++ TP-Typescript/src/personne.ts
@@ -1,15 +1,24 @@
 class Personne{
-    numero : number = 0;
-    nom : string ="!";
-    prenom : string = "!";
-    age : number = 0;
+    numero : number ;
+    prenom : string ;
+    nom : string ;
+    age : number ;
     
+    constructor(numero: number = 0, prenom : string = "?" , nom : string = "?", age : number = 0){
+        this.numero =numero;
+        this.prenom = prenom;
+        this.nom = nom;
+        this.age = age;
+
+    }
 
     incrementerAge(){
         this.age++;
     }
 }
 
+let p0 = new Personne(2, 'ramzi', 'hammed', 30);
+console.log(p0);
 let p1 : Personne  = new Personne();
 p1.nom = 'Rachid';
 p1.prenom = 'zine';
